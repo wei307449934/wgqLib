cad3ff30d8973a3632da9bd89cbcd811a7f18018
WGQ666 <419801324@qq.com>
2017-06-12 00:48:52 +0800

    初始化

---
+++ ww/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ww
+//
+//  Created by macbook on 2017/6/12.
+//  Copyright © 2017年 魏国清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ww/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ww
+//
+//  Created by macbook on 2017/6/12.
+//  Copyright © 2017年 魏国清. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
